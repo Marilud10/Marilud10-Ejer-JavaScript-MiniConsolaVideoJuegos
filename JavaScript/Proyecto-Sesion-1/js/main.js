6b3898c4fec7298b0d33613c1988a1cc27fb9113
Marilud Uribe <mariludft10@gmail.com>
2026-06-16 14:18:31 -0500

    commit clase

--- JavaScript/Proyecto-Sesion-1/js/main.js
+++ JavaScript/Proyecto-Sesion-1/js/main.js
@@ -1,41 +1,32 @@
-// Variables
 let nombreProducto = " teclado gamer ";
 let precio = 120000;
 let cantidad = 2;
 let descuento = 10000;
 let disponible = true;
 
-// Nombre limpio
 let nombreLimpio = nombreProducto.trim();
 
-// Nombre en mayúsculas
 let nombreMayusculas = nombreLimpio.toUpperCase();
 
-// Validar palabra clave
 let contieneGamer = nombreLimpio.toLowerCase().includes("gamer");
 
-// Función para calcular subtotal
 function calcularSubtotal(precio, cantidad) {
     return precio * cantidad;
 }
 
-// Función para calcular total
 function calcularTotal(subtotal, descuento) {
     return subtotal - descuento;
 }
 
-// Cálculos
 let subtotal = calcularSubtotal(precio, cantidad);
 let total = calcularTotal(subtotal, descuento);
 
-// Array de categorías
 let categorias = [
     "Periféricos",
     "Gaming",
     "Tecnología"
 ];
 
-// Objeto producto
 let producto = {
     nombre: nombreLimpio,
     precio: precio,
@@ -44,17 +35,14 @@ let producto = {
     categorias: categorias
 };
 
-// Mostrar información del array
 console.log("Primera categoría:", categorias[0]);
 console.log("Cantidad de categorías:", categorias.length);
 
-// Mostrar propiedades del objeto
 console.log("Objeto producto:");
 console.log(producto.nombre);
 console.log(producto.precio);
 console.log(producto.disponible);
 
-// Resumen final
 console.log("------ RESUMEN DE COMPRA ------");
 console.log("Producto:", nombreMayusculas);
 console.log("Precio unitario:", precio);
@@ -65,7 +53,6 @@ console.log("Total a pagar:", total);
 console.log("Producto disponible:", disponible);
 console.log("Contiene palabra clave gamer:", contieneGamer);
 
-// Reto adicional
 if (total >= 200000 && disponible) {
     console.log("Aplica envío gratis");
 } else {
